0a41f1bc6d11d3dfcd14344ee2e73cbbaf6b75b8
eastend-street <ss_1322irf@icloud.com>
2019-09-26 17:50:53 -0700

    fixed indentation used by Prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
-import Header from './components/Header/Header';
-import * as serviceWorker from './serviceWorker';
-import { Reset } from 'styled-reset'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App/App";
+import Header from "./components/Header/Header";
+import * as serviceWorker from "./serviceWorker";
+import { Reset } from "styled-reset";
 
 ReactDOM.render(
-    <React.Fragment>
-        <Reset />
-        <Header />
-        <App />
-    </React.Fragment>
-    , document.getElementById('root'));
+  <React.Fragment>
+    <Reset />
+    <Header />
+    <App />
+  </React.Fragment>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
