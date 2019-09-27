a4f41dc7456805e8ffcfdcedccf822b3f683b072
eastend-street <ss_1322irf@icloud.com>
2019-09-26 18:17:27 -0700

    added material-ui @type, edited header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from "react";
+import styled from "styled-components";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const StyledAppBar = styled(AppBar)`
+  && {
+    color: #000;
+    background-color: #fff;
+  }
+`;
 
 const App: React.FC = () => {
   return (
-    <AppBar position="static">
-    <Toolbar>
-      <IconButton edge="start" color="inherit" aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <Typography variant="h6">
-        News
-      </Typography>
-      <Button color="inherit">Login</Button>
-    </Toolbar>
-  </AppBar>
+    <StyledAppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6">Programming Tube</Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </StyledAppBar>
   );
-}
+};
 
 export default App;
