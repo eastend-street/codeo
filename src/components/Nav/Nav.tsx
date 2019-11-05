978a408a71f292472f84c4d50e7cd33d0137eb3c
eastend-street <ss_1322irf@icloud.com>
2019-11-04 16:50:27 -0800

    added list on nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
-
-`
+const Container = styled.div``;
 
 const Nav: React.FC = () => {
   return (
-    <Container>
-      <div>React</div>
+    <Container className="scroll-menu">
+      <ul className="scroll-menu-inner">
+        <li>React</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Python</li>
+        <li>Ruby</li>
+        <li>How to learn</li>
+        <li>Carrer</li>
+        <li>Most popular</li>
+      </ul>
     </Container>
   );
 };
