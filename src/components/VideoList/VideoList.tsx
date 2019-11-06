017c502c03286f4e035834ad3c72e7481e5b76d6
eastend-street <ss_1322irf@icloud.com>
2019-11-05 19:22:48 -0800

    added axios

--- src/components/VideoList/VideoList.tsx
+++ src/components/VideoList/VideoList.tsx
@@ -1,18 +1,22 @@
-import React, { useEffect, useContext }  from "react";
+import React, { useEffect, useContext } from "react";
 import AppContext from "../../contexts/AppContext";
+import { GET_VIDEOS } from "../../actions";
 
 import styled from "styled-components";
 import Grid from "@material-ui/core/Grid";
 import Video from "../Video/Video";
-
 
 const Content = styled.div``;
 
 const VideoList: React.FC = () => {
   const { dispatch } = useContext(AppContext);
   useEffect(() => {
-    ;
+    console.log('aaaaa');
+    dispatch({
+      type: GET_VIDEOS
+    });
   });
+
   return (
     <Content>
       <Grid container>
