fd3ef6487bec689d49c7b7c12147546bc9c305f8
eastend-street <ss_1322irf@icloud.com>
2019-11-07 14:21:18 -0800

    deleted getvideos function on actions/index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,2 @@
 
 export const GET_VIDEOS = "GET_VIDEOS";
-
-// export const getVideos = () => async dispatch => {
-//   console.log("aaaaaaaaaaa");
-//   const response = await axios({
-//     method: "get",
-//     url: "https://www.googleapis.com/youtube/v3/react",
-//     dataType: "json",
-//     data: {
-//       key: process.env.YOUTUBE_API_KEY
-//     }
-//   });
-//   console.log(response);
-//   dispatch({ type: GET_VIDEOS, response });
-// };
