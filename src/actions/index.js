8ca6c9e459d25ebf74efe5ec514801bf35e4838a
eastend-street <ss_1322irf@icloud.com>
2019-11-07 14:20:28 -0800

    implemented youtube api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,16 @@
-import axios from "axios";
 
 export const GET_VIDEOS = "GET_VIDEOS";
 
-export const getVideos = dispatch => async dispatch => {
-  console.log("saaaa");
-  const response = await axios({
-    method: "get",
-    url: "https://www.googleapis.com/youtube/v3/react",
-    dataType: "json",
-    data: {
-      key: process.env.YOUTUBE_API_KEY
-    }
-  });
-  console.log(response);
-  dispatch({ type: GET_VIDEOS, response });
-};
+// export const getVideos = () => async dispatch => {
+//   console.log("aaaaaaaaaaa");
+//   const response = await axios({
+//     method: "get",
+//     url: "https://www.googleapis.com/youtube/v3/react",
+//     dataType: "json",
+//     data: {
+//       key: process.env.YOUTUBE_API_KEY
+//     }
+//   });
+//   console.log(response);
+//   dispatch({ type: GET_VIDEOS, response });
+// };
--- src/components/VideoList/VideoList.tsx
+++ src/components/VideoList/VideoList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext } from "react";
 import AppContext from "../../contexts/AppContext";
-import { getVideos } from "../../actions";
+import axios from "axios";
+import { GET_VIDEOS } from "../../actions";
 
 import styled from "styled-components";
 import Grid from "@material-ui/core/Grid";
@@ -11,7 +12,20 @@ const Content = styled.div``;
 const VideoList: React.FC = () => {
   const { dispatch } = useContext(AppContext);
   useEffect(() => {
-    getVideos(dispatch);
+    async function getVideos() {
+      const response = await axios({
+        method: "get",
+        url: "https://www.googleapis.com/youtube/v3/search",
+        params: {
+          q: "react",
+          key: process.env.REACT_APP_YOUTUBE_API_KEY,
+          part: "snippet"
+        }
+      });
+      dispatch({ type: GET_VIDEOS, response });
+    }
+
+    getVideos();
   });
 
   return (
