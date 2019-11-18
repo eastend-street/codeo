876eb20acd26a7d490e51d0e2e953d7517e529fd
eastend-street <ss_1322irf@icloud.com>
2019-11-17 23:36:09 -0800

    added try catch

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -2,16 +2,20 @@ import axios from "axios";
 
 export const GET_VIDEOS = "GET_VIDEOS";
 
-export const getVideos = async (param:string, dispatch: any) => {
-  const response = await axios({
-    method: "get",
-    url: "https://www.googleapis.com/youtube/v3/search",
-    params: {
-      q: param,
-      key: process.env.REACT_APP_YOUTUBE_API_KEY,
-      part: "snippet",
-      maxResults: 8
-    }
-  });
-  dispatch({ type: GET_VIDEOS, response });
+export const getVideos = async (param: string, dispatch: any) => {
+  try {
+    const response = await axios({
+      method: "get",
+      url: "https://www.googleapis.com/youtube/v3/search",
+      params: {
+        q: param,
+        key: process.env.REACT_APP_YOUTUBE_API_KEY,
+        part: "snippet",
+        maxResults: 8
+      }
+    });
+    dispatch({ type: GET_VIDEOS, response });
+  } catch (error) {
+    console.error(error);
+  }
 };
