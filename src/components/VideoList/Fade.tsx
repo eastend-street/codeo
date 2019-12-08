82fed841d09b5de3047a120cc3d41610b9b3066c
eastend-street <ss_1322irf@icloud.com>
2019-12-07 17:40:07 -0800

    changed jsx to tsx on fade

--- src/components/VideoList/Fade.tsx
+++ src/components/VideoList/Fade.tsx
@@ -1,38 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import styled, { keyframes } from "styled-components";
 
 const fadeIn = keyframes`
   0% {
-    height: 0;
+    /* height: 0; */
+    opacity: 0
   }
   50% {
-    height: 50%;
+    /* height: 50%; */
+    opacity: 0.5;
   }
   100% {
-    height: 100%;
+    /* height: 100%; */
+    opacity: 1;
   }
 `;
 
 const fadeOut = keyframes`
   0% {
-    height: 100%
+    /* height: 100%; */
+    opacity: 1;
   }
   50% {
-    height: 50%;
+    /* height: 50%; */
+    opacity: 0.5;
   }
   100% {
-    height: 0%;
+    /* height: 0%; */
+    opacity: 0;
+
   }
 `;
 
-const StyledFade = styled.div`
-  background-color: grey;
-  color: #fff;
+const StyledFade = styled.div<{ show: boolean }>`
   overflow: hidden;
   animation: ${props => (props.show ? fadeIn : fadeOut)} 0.3s linear forwards;
 `;
 
-const Fade = ({ show, children }) => {
+type FadeProps = {
+  show: any;
+  children: ReactNode;
+};
+
+const Fade = ({ show, children }: FadeProps) => {
   const [render, setRender] = useState(show);
 
   useEffect(() => {
