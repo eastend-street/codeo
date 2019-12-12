295dd379ee98935776b64878581130bccb51126c
eastend-street <ss_1322irf@icloud.com>
2019-12-11 21:09:20 -0800

    changed nav space

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -7,7 +7,6 @@ import { getVideos } from "../../actions";
 import NavJSON from "../../data/nav.json";
 
 const Container = styled.div`
-  position: relative;
 `;
 
 const NavUl = styled.ul`
@@ -22,27 +21,17 @@ const NavUl = styled.ul`
 const NavLi = styled.li``;
 
 const StyledNavButton = styled.div<{ selected: boolean }>`
-  margin: 0 1rem;
-  padding-bottom: 0.3rem;
-  /* border-bottom: ${props =>
-    props.selected ? "0.1rem solid #fff" : "none"}; */
+  margin: 0 0.5rem;
+  padding: 0.5rem 1rem;
+  /* border: ${props => (props.selected ? "0.0625rem solid #fff" : "0.0625rem solid #fff")}; */
+  border-radius: 1rem;
   opacity: ${props => (props.selected ? "1" : "0.5")};
   transition: 0.7s;
   &:hover {
     opacity: 1;
     cursor: pointer;
-  }
-`;
 
-const Underline = styled.span<{ underlineLeft: number }>`
-  background: #fff;
-  height: 0.1rem;
-  display: block;
-  position: absolute;
-  left: ${props => props.underlineLeft}px;
-  bottom: 0;
-  width: 2rem;
-  transition: 0.5s;
+  }
 `;
 
 type NavData = {
@@ -87,7 +76,6 @@ const Nav: React.FC = () => {
   return (
     <Container>
       <NavUl>{renderNav()}</NavUl>
-      <Underline underlineLeft={underlineLeft} />
     </Container>
   );
 };
